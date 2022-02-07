773e652a670b5dc75ad7144a91c857161d9a0172
qkrxodud00 <qkrxodud00@gmail.com>
2022-02-07 21:46:56 +0900

    *퀴즈 완료시 Alert 문구  -  Alert를 쓸때 MaterialApp 오류가 발생 , 이유 : 빌드전에 showDialog 가 실행되면서 초기화가 안됬다고 함   ==> 해결 방법 runApp 실행시 MaterialApp을 위에 작성

--- lib/ox_quiz_version_2.dart
+++ lib/ox_quiz_version_2.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_ox_quiz/quiz.dart';
 
 void main() {
-  runApp(const OxQuiz());
+  runApp(const MaterialApp(home: OxQuiz()));
 }
 
 class OxQuiz extends StatefulWidget {
@@ -18,74 +18,101 @@ class _OxQuizState extends State<OxQuiz> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.grey.shade900,
-        body: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Expanded(
-                    flex: 5,
-                    child: Center(
-                        child: Text(
-                          quiz.getQuestion(),
-                          style: const TextStyle(color: Colors.white),
-                        ))),
-                Expanded(
-                    flex: 2,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Expanded(
-                            child: TextButton(
-                              onPressed: () {
-                                setState(() {
-                                  quiz.checkQuestion(true);
-                                  quiz.nextQuestion();
-                                });
-                              },
-                              child: Container(
-                                  color: Colors.green,
-                                  child: Center(
-                                      child: Icon(
-                                        Icons.check_circle_outline,
-                                        color: Colors.grey.shade900,
-                                      ))),
-                            )),
-                        const SizedBox(
-                          width: 5,
-                        ),
-                        Expanded(
-                            child: TextButton(
-                              onPressed: () {
-                                setState(() {
-                                  quiz.checkQuestion(false);
-                                  quiz.nextQuestion();
-                                });
-                              },
-                              child: Container(
-                                  color: Colors.red,
-                                  child: Center(
-                                      child: Icon(
-                                        Icons.highlight_off,
-                                        color: Colors.grey.shade900,
-                                      ))),
-                            )),
-                      ],
-                    )),
-                const SizedBox(
-                  height: 5,
-                ),
-                Row(children: quiz.getIcons())
-              ],
-            ),
+    return Scaffold(
+      backgroundColor: Colors.grey.shade900,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Expanded(
+                  flex: 5,
+                  child: Center(
+                      child: Text(
+                    quiz.getQuestion(),
+                    style: const TextStyle(color: Colors.white),
+                  ))),
+              Expanded(
+                  flex: 2,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Expanded(
+                          child: TextButton(
+                        onPressed: () {
+                          setState(() {
+                            quiz.checkQuestion(true);
+                            quiz.nextQuestion();
+                            if (quiz.checkLastQuiz()) {
+                              alert(context);
+                            }
+                          });
+                        },
+                        child: Container(
+                            color: Colors.green,
+                            child: Center(
+                                child: Icon(
+                              Icons.check_circle_outline,
+                              color: Colors.grey.shade900,
+                            ))),
+                      )),
+                      const SizedBox(
+                        width: 5,
+                      ),
+                      Expanded(
+                          child: TextButton(
+                        onPressed: () {
+                          setState(() {
+                            quiz.checkQuestion(false);
+                            quiz.nextQuestion();
+                            if (quiz.checkLastQuiz()) {
+                              alert(context);
+                            }
+                          });
+                        },
+                        child: Container(
+                            color: Colors.red,
+                            child: Center(
+                                child: Icon(
+                              Icons.highlight_off,
+                              color: Colors.grey.shade900,
+                            ))),
+                      )),
+                    ],
+                  )),
+              const SizedBox(
+                height: 5,
+              ),
+              Row(children: quiz.getIcons())
+            ],
           ),
         ),
       ),
     );
+  }
+
+  // 다이얼로그
+  Future<void> alert(BuildContext context) {
+    return showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text("퀴즈가 끝났습니다."),
+            actions: [
+              TextButton(
+                  onPressed: () {
+                    Navigator.pop(context, false);
+                    setState(() {
+                      quiz.clear();
+                    });
+                  },
+                  child: const Text('확인')),
+            ],
+          );
+        }).then((result) {
+      result = result;
+    });
   }
 }
